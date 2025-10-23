419158d4b0036866b3be153ec3493e447349c2f3
cKohl10 <carson.kohlbrenner@gmail.com>
2025-10-23 17:55:30 -0600

    Naming conventions

--- setup.py
+++ setup.py
@@ -17,17 +17,24 @@ def get_data_files_recursive(base_dir, target_dir):
             files.append((os.path.dirname(target_path), [file_path]))
     return files
 
+# Build data_files list
+data_files = [
+    ('share/ament_index/resource_index/packages',
+        ['resource/' + package_name]),
+    ('share/' + package_name, ['package.xml']),
+    (os.path.join('share', package_name, 'launch'), glob('launch/*')),
+    (os.path.join('share', package_name, 'config'), glob('config/*')),
+]
+data_files.extend(get_data_files_recursive('meshes', os.path.join('share', package_name, 'meshes')))
+data_files.extend(get_data_files_recursive('urdf', os.path.join('share', package_name, 'urdf')))
+data_files.extend(get_data_files_recursive('robots', os.path.join('share', package_name, 'robots')))
+data_files.extend(get_data_files_recursive('end_effectors', os.path.join('share', package_name, 'end_effectors')))
+
 setup(
     name=package_name,
     version='0.0.0',
     packages=find_packages(exclude=['test']),
-    data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name, 'launch'), glob('launch/*')),
-        (os.path.join('share', package_name, 'config'), glob('config/*')),
-    ] + get_data_files_recursive('meshes', os.path.join('share', package_name, 'meshes')) + get_data_files_recursive('urdf', os.path.join('share', package_name, 'urdf')),
+    data_files=data_files,
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='carson',
