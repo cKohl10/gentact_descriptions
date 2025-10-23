a7ffd23d278941241887ed5e3a45e8ac128d35cc
cKohl10 <carson.kohlbrenner@gmail.com>
2025-10-23 10:12:33 -0600

    fixed urdfs

--- setup.py
+++ setup.py
@@ -1,6 +1,21 @@
 from setuptools import find_packages, setup
+from glob import glob
+import os
 
 package_name = 'gentact_descriptions'
+
+# Function to recursively get all files in a directory
+def get_data_files_recursive(base_dir, target_dir):
+    files = []
+    for root, dirs, filenames in os.walk(base_dir):
+        for filename in filenames:
+            file_path = os.path.join(root, filename)
+            # Calculate the relative path from base_dir
+            rel_path = os.path.relpath(file_path, base_dir)
+            # Create the target path
+            target_path = os.path.join(target_dir, rel_path)
+            files.append((os.path.dirname(target_path), [file_path]))
+    return files
 
 setup(
     name=package_name,
@@ -10,12 +25,14 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-    ],
+        (os.path.join('share', package_name, 'launch'), glob('launch/*')),
+        (os.path.join('share', package_name, 'config'), glob('config/*')),
+    ] + get_data_files_recursive('meshes', os.path.join('share', package_name, 'meshes')) + get_data_files_recursive('urdf', os.path.join('share', package_name, 'urdf')),
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='caleb',
+    maintainer='carson',
     maintainer_email='carson.kohlbrenner@gmail.com',
-    description='TODO: Package description',
+    description='URDF and launch files for custom robots with skin units',
     license='Apache-2.0',
     extras_require={
         'test': [
