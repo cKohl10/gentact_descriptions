0ece27a88acb22e64376f29225a46bb7e10fdcd6
cKohl10 <carsonk.kohlbrenner@colorado.edu>
2026-09-05 16:15:27 -0600

    Added new link6

--- build/gentact_descriptions/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/carson/ros2_ws/src/gentact_descriptions/install/gentact_descriptions'
